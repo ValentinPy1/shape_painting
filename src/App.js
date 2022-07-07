4ecfa912b995eb10e856d3a39eb525e7287cb60d
Valentin PY <valentin.py@epitech.eu>
2022-07-07 15:29:54 +0200

    [ADD] image merging

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import UploadImage from './UploadImage'
-import Canva from './Canva'
+import ImageProcessing from './ImageProcessing'
 import React, { useState } from 'react'
 
 function App() {
-  const [imageUrl, setImageUrl] = useState()
+  const [imageUrl1, setImageUrl1] = useState()
+  const [imageUrl2, setImageUrl2] = useState()
 
   return (
     <div className="App">
-      <UploadImage imageUrl={imageUrl} setImageUrl={setImageUrl}></UploadImage>
-      <Canva imageUrl={imageUrl}></Canva>
+      <UploadImage imageUrl={imageUrl1} setImageUrl={setImageUrl1}></UploadImage>
+      <UploadImage imageUrl={imageUrl2} setImageUrl={setImageUrl2}></UploadImage>
+      <ImageProcessing imageUrl_1={imageUrl1} imageUrl_2={imageUrl2}/>
     </div>
   );
 }
---
+++ src/ImageProcessing.js
@@ -0,0 +1,98 @@
+import React, {useRef, useEffect} from 'react'
+
+const rdmColor = () => {
+    return "#" + Math.floor(Math.random()*16777215).toString(16)
+}
+
+function Point(x, y) {
+    this.x = x
+    this.y = y
+}
+
+const rdmPoint = (max_x, max_y) => {
+    return new Point(Math.random() * max_x, Math.random() * max_y)
+}
+
+function Triangle(p0, p1, p2, color) {
+    this.p0 = p0
+    this.p1 = p1
+    this.p2 = p2
+    this.color = color
+    this.draw = (ctx) => {
+        ctx.beginPath()
+        ctx.moveTo(p0.x, p0.y)
+        ctx.lineTo(p1.x, p1.y)
+        ctx.lineTo(p2.x, p2.y)
+        ctx.fillStyle = color
+        console.log(color)
+        ctx.fill()
+    }
+}
+
+const rdmTriangle = (max_x, max_y) => {
+    return new Triangle(rdmPoint(max_x, max_y), rdmPoint(max_x, max_y), rdmPoint(max_x, max_y), rdmColor())
+}
+
+export default function ImageProcessing({imageUrl_1, imageUrl_2, props}) {
+
+    const bruteRef_1 = useRef(null)
+    const bruteRef_2 = useRef(null)
+    const diffRef = useRef(null)
+
+    function draw_image(ref, imageUrl, imageData_1, imageData_2, width, height) {
+        const canvas = ref.current
+        canvas.width = width
+        canvas.height = height
+        const ctx = canvas.getContext('2d')
+
+        if (imageUrl === undefined) return
+        const image = new Image()
+        image.src = imageUrl
+        image.onload = () => {
+            ctx.drawImage(image, 0, 0, width, height)
+            rdmTriangle(width, height).draw(ctx)
+            imageData_1.data = ctx.getImageData(0, 0, width, height)
+            draw_diff(diffRef, imageData_1.data, imageData_2.data, width, height)
+        }
+    }
+
+    function draw_diff(ref, iD1, iD2, width, height) {
+        const canvas = ref.current
+        canvas.width = width
+        canvas.height = height
+        const ctx = canvas.getContext('2d')
+        ctx.clearRect(0, 0, width, height)
+        let imageData = ctx.getImageData(0, 0, width, height)
+
+        if (iD1 === undefined || iD2 === undefined) {
+            return
+        }
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                let index = (x + y * width) * 4
+                imageData.data[index + 0] = Math.abs(iD1.data[index + 0] - iD2.data[index + 0])
+                imageData.data[index + 1] = Math.abs(iD1.data[index + 1] - iD2.data[index + 1])
+                imageData.data[index + 2] = Math.abs(iD1.data[index + 2] - iD2.data[index + 2])
+                imageData.data[index + 3] = 255
+            }
+        }
+        ctx.putImageData(imageData, 0, 0)
+    }
+
+    useEffect(() => {
+        let imageData_1 = {'data': undefined}
+        let imageData_2 = {'data': undefined}
+        let width = 512
+        let height = 512
+        draw_image(bruteRef_1, imageUrl_1, imageData_1, imageData_2, width, height)
+        draw_image(bruteRef_2, imageUrl_2, imageData_2, imageData_1, width, height)
+    }, [draw_image, draw_diff])
+
+    return (
+        <div>
+            <canvas ref={bruteRef_1} {...props}/>
+            <canvas ref={bruteRef_2} {...props}/>
+            <canvas ref={diffRef} {...props}/>
+        </div>
+    );
+}
