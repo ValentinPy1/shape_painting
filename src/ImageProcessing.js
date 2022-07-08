b036e15683323781eabff982d81b51bb98662421
Valentin PY <valentin.py@epitech.eu>
2022-07-08 10:15:26 +0200

    [ADD] Triangle Array object and batches

--- src/ImageProcessing.js
+++ src/ImageProcessing.js
@@ -1,16 +1,39 @@
 import React, {useRef, useEffect} from 'react'
 
-const rdmColor = () => {
-    return "#" + Math.floor(Math.random()*16777215).toString(16)
+const rdmExp = (dispersion) => {
+    if (Math.random() > 0.5)
+        return Math.pow((-1 / ((Math.random() * 0.999) - 1)), dispersion)
+    return -(Math.pow((-1 / ((Math.random() * 0.999) - 1)), dispersion))
 }
 
 function Point(x, y) {
     this.x = x
     this.y = y
+    this.mutate = (disp) => {
+        console.log(rdmExp(disp))
+        this.x += rdmExp(disp)
+        this.y += rdmExp(disp)
+    }
 }
 
 const rdmPoint = (max_x, max_y) => {
     return new Point(Math.random() * max_x, Math.random() * max_y)
+}
+
+function Color() {
+    this.r = Math.random() * 255
+    this.g = Math.random() * 255
+    this.b = Math.random() * 255
+    this.a = Math.random()
+    this.string = () => {
+        return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`
+    }
+    this.mutate = (disp) => {
+        this.r += rdmExp(disp)
+        this.g += rdmExp(disp)
+        this.b += rdmExp(disp)
+        this.a += rdmExp(disp) / 255
+    }
 }
 
 function Triangle(p0, p1, p2, color) {
@@ -23,14 +46,47 @@ function Triangle(p0, p1, p2, color) {
         ctx.moveTo(p0.x, p0.y)
         ctx.lineTo(p1.x, p1.y)
         ctx.lineTo(p2.x, p2.y)
-        ctx.fillStyle = color
-        console.log(color)
+        ctx.fillStyle = color.string()
         ctx.fill()
+    }
+    this.mutate = (disp) => {
+        this.p0.mutate(disp)
+        this.p1.mutate(disp)
+        this.p2.mutate(disp)
+        this.color.mutate()
     }
 }
 
 const rdmTriangle = (max_x, max_y) => {
-    return new Triangle(rdmPoint(max_x, max_y), rdmPoint(max_x, max_y), rdmPoint(max_x, max_y), rdmColor())
+    return new Triangle(rdmPoint(max_x, max_y), rdmPoint(max_x, max_y), rdmPoint(max_x, max_y), new Color())
+}
+
+function TriangleArray(width, height, count) {
+    this.triangles = []
+    for (let i = 0; i < count; ++i)
+        this.triangles.push(rdmTriangle(width, height))
+    this.mutate = (disp) => {
+        for (let triangle of this.triangles)
+            triangle.mutate(disp)
+    }
+    this.draw = (ctx) => {
+        for (let triangle of this.triangles)
+            triangle.draw(ctx)
+    }
+    this.fitness = (ctx) => {
+        let fitness = 0
+        return fitness
+    }
+}
+
+function Population(ctx, batch_size, batch_count) {
+    this.batches = []
+    for (let i = 0; i < batch_count; ++i)
+        this.batches.push(new TriangleArray(ctx.width, ctx.height, batch_size))
+    this.mutate = (disp) => {
+        for (let batche of this.batches)
+            batche.mutate(disp)
+    }
 }
 
 export default function ImageProcessing({imageUrl_1, imageUrl_2, props}) {
@@ -50,7 +106,11 @@ export default function ImageProcessing({imageUrl_1, imageUrl_2, props}) {
         image.src = imageUrl
         image.onload = () => {
             ctx.drawImage(image, 0, 0, width, height)
-            rdmTriangle(width, height).draw(ctx)
+            // const ta = new TriangleArray(width, height, 10)
+            // for (let i = 0; i < 1; ++i) {
+            //     ta.draw(ctx)
+            //     ta.mutate(0.5)
+            // }
             imageData_1.data = ctx.getImageData(0, 0, width, height)
             draw_diff(diffRef, imageData_1.data, imageData_2.data, width, height)
         }
